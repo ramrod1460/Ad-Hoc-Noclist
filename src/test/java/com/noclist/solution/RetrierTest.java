388be97c866f5a32985e453b26ab3c7432889a3f
Rod Madden <rod.madden@pricefx.com>
2021-10-11 17:02:56 -0400

    Merge Test 2

--- src/test/java/com/noclist/solution/RetrierTest.java
+++ src/test/java/com/noclist/solution/RetrierTest.java
@@ -6,6 +6,9 @@ import java.util.concurrent.atomic.AtomicInteger;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+/**
+* Merge test 2
+*/
 class RetrierTest {
 
     @Test
